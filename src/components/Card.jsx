42d5da264496eed0801ea4f0ffe3ab8131eaeb45
zenishshr <zenishshr@gmail.com>
2021-05-10 09:07:30 +0545

    Make width as a default prop in card.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 // Children accepts JSX data passed inside the component
 // <Card><p>JSX data....</p></Card>
-export default function Card({ children }) {
+export default function Card({ width = "100%", children }) {
   return (
-    <div style={{ padding: 20, border: "1px solid #ddd", marginBottom: 20 }}>
+    <div
+      style={{ padding: 20, border: "1px solid #ddd", marginBottom: 20, width }}
+    >
       {children}
     </div>
   );
